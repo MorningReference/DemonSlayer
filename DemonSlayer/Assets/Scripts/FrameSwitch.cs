46cfbd889b9d75771456a888ca1aa6c42155c203
MorningReference <aricjeon@gmail.com>
2020-06-23 16:36:50 -0700

    FINALLY merged

---
+++ DemonSlayer/Assets/Scripts/FrameSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameSwitch : MonoBehaviour
+{
+    public GameObject frame1;
+    public GameObject frame2;
+    public bool isForward = false;
+    public bool isBack = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (frame1.active == true)
+        {
+            isForward = true;
+            frame1.SetActive(false);
+            frame2.SetActive(true);
+        }
+        else if (frame1.active == false)
+        {
+            frame1.SetActive(true);
+            isBack = true;
+            frame2.SetActive(false);
+        }
+    }
+}
---
+++ DemonSlayer/Assets/Scripts/ParallaxCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxCamera : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+    [SerializeField] private float oldPosition;
+
+    public FrameSwitch edge;
+    void Start()
+    {
+        oldPosition = transform.position.x;
+    }
+    void Update()
+    {
+        if (transform.position.x != oldPosition)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);
+            }
+            oldPosition = transform.position.x;
+        }
+        if (edge.isForward == true)
+        {
+            oldPosition = 28;
+            edge.isForward = false;
+        }
+        else if (edge.isBack == true)
+        {
+            oldPosition = 15;
+            edge.isBack = false;
+        }
+    }
+}
