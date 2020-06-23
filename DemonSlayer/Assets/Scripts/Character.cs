2497f5e084f50bd81b8d5953ecc75322b7db7e2d
LEVI ____ <blanklevi@gmail.com>
2020-06-23 15:55:19 -0700

    first commit Levi Branch

---
+++ DemonSlayer/Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character
+{
+    [SerializeField] string Name;
+    [SerializeField] int Strength;
+    [SerializeField] int Stamina;
+    [SerializeField] int Endurance;
+    [SerializeField] int Dexterity;
+    [SerializeField] int Agility;
+    [SerializeField] int Luck;
+    [SerializeField] int CurrentHP;
+    [SerializeField] int MaxHP;
+    [SerializeField] int CurrentBreathCapacity;
+    [SerializeField] int MaxBreathCapacity;
+    [SerializeField] int CurrentTensionMeter;
+    [SerializeField] float Attack;
+    [SerializeField] float Defense;
+    [SerializeField] float AttackSpeed;
+    [SerializeField] float CriticalRate;
+    [SerializeField] float CriticalDamage;
+
+}
